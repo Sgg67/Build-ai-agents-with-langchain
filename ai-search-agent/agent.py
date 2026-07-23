f389cc93046d4fb5db7456bc7cbb74b0e8c94508
sage <sage.yanoff@tcs.com>
2026-07-23 17:02:43 -0400

    update agent file

---
+++ ai-search-agent/agent.py
@@ -0,0 +1,7 @@
+# import necessary modules
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
+# load the env file
+load_dotenv()
+
